5f296ba911f982f31db7fa553c9397ce29df805a
Alexander Terekhov <cahdro2155@gmail.com>
2025-02-16 21:12:33 +0300

    Завершил курс

--- forms_project/settings.py
+++ forms_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
